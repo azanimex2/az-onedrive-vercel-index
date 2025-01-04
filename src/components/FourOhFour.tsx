953900a10e0a69acde0e4575a700322dae355f53
azanimex2 <131632743+azanimex2@users.noreply.github.com>
2025-01-03 21:10:22 -0300

    Update FourOhFour.tsx

--- src/components/FourOhFour.tsx
+++ src/components/FourOhFour.tsx
@@ -1,7 +1,12 @@
 import Image from 'next/image'
 import { Trans } from 'next-i18next'
+import { useRouter } from 'next/router'
 
 const FourOhFour: React.FC<{ errorMsg: string }> = ({ errorMsg }) => {
+  const router = useRouter()
+  const currentPath = router.asPath
+  const backupUrl = `https://series-am2.az-animex.com${currentPath}`
+
   return (
     <div className="my-12">
       <div className="mx-auto w-1/3">
@@ -9,7 +14,7 @@ const FourOhFour: React.FC<{ errorMsg: string }> = ({ errorMsg }) => {
       </div>
       <div className="mx-auto mt-6 max-w-xl text-gray-500">
         <div className="mb-8 text-xl font-bold">
-          El servidor llegó a su límite de cuota diaria. Tendrás que esperar hasta las 21hs UTC-3 para su reinicio o ir al servidor de respaldo mediante <a className="text-blue-600 hover:text-blue-700 hover:underline" href="https://series-am2.az-animex.com/es/" target="_blank" rel="noopener noreferrer">este enlace</a>.
+          El servidor llegó a su límite de cuota diaria. Tendrás que esperar hasta las 21hs UTC-3 para su reinicio o ir al servidor de respaldo mediante <a className="text-blue-600 hover:text-blue-700 hover:underline" href={backupUrl} target="_blank" rel="noopener noreferrer">este enlace</a>.
         </div>
       </div>
     </div>
